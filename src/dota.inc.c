511cb8d18db9f5799beb0e78986666b2d3deb0f4
amunra <amunra@users.noreply.github.com>
2022-05-27 11:59:56 +0100

    Visual Studio Support (MSVC)

--- src/dota.inc.c
+++ src/dota.inc.c
@@ -1,8 +1,32 @@
+#include "build_env.h"
+
 #define IEEE_8087 1
+
 #define MALLOC aborting_malloc
 #define REALLOC aborting_realloc
-#pragma GCC diagnostic ignored "-Wsign-compare"
-#pragma GCC diagnostic ignored "-Wimplicit-fallthrough="
+
+#if defined(COMPILER_MSVC)
+#    pragma warning( push )
+#    pragma warning( disable : 4244 )
+#    pragma warning( disable : 4273 )
+#    pragma warning( disable : 4146 )
+#    pragma warning( disable : 4334 )
+#    pragma warning( disable : 4706 )
+#    pragma warning( disable : 4701 )
+#    pragma warning( disable : 4703 )
+#endif
+
+#if defined(COMPILER_GNUC)
+#    pragma GCC diagnostic ignored "-Wsign-compare"
+#    pragma GCC diagnostic ignored "-Wimplicit-fallthrough="
+#endif
+
 #include "dota.c"
-#pragma GCC diagnostic pop
-#pragma GCC diagnostic pop
+
+#if defined(COMPILER_GNUC)
+#    pragma GCC diagnostic pop
+#    pragma GCC diagnostic pop
+#endif
+
+#undef MALLOC
+#undef REALLOC
