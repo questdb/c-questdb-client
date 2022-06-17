d990069e50ff8628e831686413707083a39cebba
Adam Cimarosti <cimarosti@gmail.com>
2022-06-18 00:09:43 +0100

    test: Better debugging output for CI

--- system_test/fixture.py
+++ system_test/fixture.py
@@ -36,6 +36,7 @@ import time
 import socket
 import atexit
 import shlex
+import textwrap
 import urllib.request
 import urllib.parse
 import urllib.error
@@ -238,7 +239,12 @@ class QuestDbFixture:
                 timeout_sec=20,
                 msg='Timed out waiting for HTTP service to come up.')
         except:
-            sys.stderr.write(f'Failed to start, see: {self._log_path}\n')
+            sys.stderr.write(f'Failed to start, see full log: {self._log_path}\n')
+            with open(self._log_path, 'r', encoding='utf-8') as log_file:
+                lines = log_file.readlines()
+                buf = '\n'.join(lines[-100:])
+                sys.stderr.write(textwrap.indent(buf, '    '))
+                sys.stderr.write('\n')
             raise
 
         atexit.register(self.stop)
