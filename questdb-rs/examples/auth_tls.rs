bfc22b6ea30d4b6ccd25a938794bf805b66d69a3
amunra <amunra@users.noreply.github.com>
2022-08-12 11:06:06 +0100

    doc: Added rust cloud example, indexed remaining examples.

--- questdb-rs/examples/auth_tls.rs
+++ questdb-rs/examples/auth_tls.rs
@@ -2,14 +2,23 @@ use questdb::{
     Result,
     ingress::{
         Buffer,
-        SenderBuilder}};
+        SenderBuilder,
+        Tls,
+        CertificateAuthority}};
 
 fn main() -> Result<()> {
     let host: String = std::env::args().nth(1)
         .unwrap_or("localhost".to_string());
     let port: u16 = std::env::args().nth(2)
         .unwrap_or("9009".to_string()).parse().unwrap();
-    let mut sender = SenderBuilder::new(host, port).connect()?;
+    let mut sender = SenderBuilder::new(host, port)
+        .auth(
+            "testUser1",                                    // kid
+            "5UjEMuA0Pj5pjK8a-fa24dyIf-Es5mYny3oE_Wmus48",  // d
+            "fLKYEaoEb9lrn3nkwLDA-M_xnuFOdSt9y0Z7_vWSHLU",  // x
+            "Dt5tbS1dEDMSYfym3fgMv0B99szno-dFc1rYF9t0aac")  // y
+        .tls(Tls::Enabled(CertificateAuthority::WebpkiRoots))
+        .connect()?;
     let mut buffer = Buffer::new();
     buffer
         .table("sensors")?
