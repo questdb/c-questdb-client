8c1d9b94f5abc75afa4a0c87fc1353c7337cde24
Adam Cimarosti <cimarosti@gmail.com>
2025-05-15 16:18:26 +0100

    fixed broken C example

--- examples/line_sender_c_example_auth.c
+++ examples/line_sender_c_example_auth.c
@@ -11,7 +11,7 @@ static bool example(const char* host, const char* port)
     line_sender_buffer* buffer = NULL;
     char* conf_str = concat(
         "tcp::addr=", host, ":", port, ";"
-        "protocol_version=2"
+        "protocol_version=2;"
         "username=admin;"
         "token=5UjEMuA0Pj5pjK8a-fa24dyIf-Es5mYny3oE_Wmus48;"
         "token_x=fLKYEaoEb9lrn3nkwLDA-M_xnuFOdSt9y0Z7_vWSHLU;"
