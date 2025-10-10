74f6f626431bd8779271e71d93f09c5d94e56559
Raphael DALMON <raphael.dalmon@gmail.com>
2025-10-10 15:47:55 +0200

    docs: add decimal in rust examples

--- questdb-rs/examples/protocol_version.rs
+++ questdb-rs/examples/protocol_version.rs
@@ -1,3 +1,6 @@
+use std::str::FromStr;
+
+use bigdecimal::BigDecimal;
 use ndarray::arr1;
 use questdb::{
     ingress::{Sender, TimestampNanos},
@@ -5,29 +8,31 @@ use questdb::{
 };
 
 fn main() -> Result<()> {
+    let price = BigDecimal::from_str("2615.54").unwrap();
+
     let mut sender = Sender::from_conf(
-        "https::addr=localhost:9000;username=foo;password=bar;protocol_version=1;",
+        "http::addr=localhost:9000;username=foo;password=bar;protocol_version=1;",
     )?;
     let mut buffer = sender.new_buffer();
     buffer
         .table("trades_ilp_v1")?
         .symbol("symbol", "ETH-USD")?
         .symbol("side", "sell")?
-        .column_f64("price", 2615.54)?
+        .column_decimal("price", &price)?
         .column_f64("amount", 0.00044)?
         .at(TimestampNanos::now())?;
     sender.flush(&mut buffer)?;
 
     // QuestDB server version 9.0.0 or later is required for `protocol_version=2` support.
     let mut sender2 = Sender::from_conf(
-        "https::addr=localhost:9000;username=foo;password=bar;protocol_version=2;",
+        "http::addr=localhost:9000;username=foo;password=bar;protocol_version=2;",
     )?;
-    let mut buffer2 = sender.new_buffer();
+    let mut buffer2 = sender2.new_buffer();
     buffer2
         .table("trades_ilp_v2")?
         .symbol("symbol", "ETH-USD")?
         .symbol("side", "sell")?
-        .column_f64("price", 2615.54)?
+        .column_decimal("price", &price)?
         .column_f64("amount", 0.00044)?
         .column_arr("location", &arr1(&[100.0, 100.1, 100.2]).view())?
         .at(TimestampNanos::now())?;
