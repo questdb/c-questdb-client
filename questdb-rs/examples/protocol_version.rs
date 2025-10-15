12882be6e2a27669a836eeebe4f76f75354903b4
Raphael DALMON <raphael.dalmon@gmail.com>
2025-10-15 18:23:02 +0200

    fix: update price column type and correct table name in protocol_version example

--- questdb-rs/examples/protocol_version.rs
+++ questdb-rs/examples/protocol_version.rs
@@ -8,8 +8,6 @@ use questdb::{
 };
 
 fn main() -> Result<()> {
-    let price = BigDecimal::from_str("2615.54").unwrap();
-
     let mut sender = Sender::from_conf(
         "http::addr=localhost:9000;username=foo;password=bar;protocol_version=1;",
     )?;
@@ -18,18 +16,19 @@ fn main() -> Result<()> {
         .table("trades_ilp_v1")?
         .symbol("symbol", "ETH-USD")?
         .symbol("side", "sell")?
-        .column_dec("price", &price)?
+        .column_f64("price", 2615.54)?
         .column_f64("amount", 0.00044)?
         .at(TimestampNanos::now())?;
     sender.flush(&mut buffer)?;
 
-    // QuestDB server version 9.0.0 or later is required for `protocol_version=2` support.
+    // QuestDB server version 9.2.0 or later is required for `protocol_version=3` support.
     let mut sender2 = Sender::from_conf(
         "http::addr=localhost:9000;username=foo;password=bar;protocol_version=3;",
     )?;
+    let price = BigDecimal::from_str("2615.54").unwrap();
     let mut buffer2 = sender2.new_buffer();
     buffer2
-        .table("trades_ilp_v2")?
+        .table("trades_ilp_v3")?
         .symbol("symbol", "ETH-USD")?
         .symbol("side", "sell")?
         .column_dec("price", &price)?
